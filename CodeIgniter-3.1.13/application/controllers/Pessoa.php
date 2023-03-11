0e1677c7afae37a15867aaae2763cd4f2a0c3140
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-03-10 23:20:26 -0300

    ok

---
+++ CodeIgniter-3.1.13/application/controllers/Pessoa.php
@@ -0,0 +1,29 @@
+<?php
+/*
+setor
+usuario
+perfil
+pessoa
+atendimento
+*/
+class Pessoa extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('index');
+    }
+    public function tela_adicionar()    {
+        
+        $this->load->view('tela_adicionar', $data);
+    }
+    public function tela_editar()    {
+        
+        $this->load->view('tela_editar', $data);
+    }
+    public function editar()    {
+    }
+    public function remover()    {
+    }
+    public function adicionar()    {        
+        // $this->load->view('tela_adicionar', $data);
+    }
+}
