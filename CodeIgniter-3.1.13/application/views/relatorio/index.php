682d4c03813cfe9c226645c5b0233db5a2f75794
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-03-12 17:08:15 -0300

    ok

--- CodeIgniter-3.1.13/application/views/relatorio/index.php
+++ CodeIgniter-3.1.13/application/views/relatorio/index.php
@@ -2,6 +2,10 @@
 
 <h2> Quantidade Por Sexo </h2>
 <?php if (count($dados)>0) { ?>
-    <!-- <?=($dados[0]->sexo == "M")? "Masculino" : "Feminino" ?>  <?=$dados[0]->qtde?> <br>
-    <?=($dados[1]->sexo == "M")? "Masculino" : "Feminino" ?>  <?=$dados[1]->qtde?> -->
+    <?php if (isset($dados[0])) { ?>
+        <?=($dados[0]->sexo == "M")? "Masculino" : "Feminino" ?>  <?=$dados[0]->qtde?> <br>
+    <?php }?>
+    <?php if (isset($dados[1])) { ?>    
+        <?=($dados[1]->sexo == "M")? "Masculino" : "Feminino" ?>  <?=$dados[1]->qtde?>
+    <?php }?>
 <?php } ?>
